5cb9228f26ccf2dd01e94c850b27f0697a7d1061
trevor-mill <trevor.million@chicagobooth.edu>
2022-01-23 18:05:07 +0000

    date formatted finished

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,12 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2020, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+today = Date.today
+year = today.year
+date = today.day
+month = today.month
+
+p "The year is: #{year}, the calendar day is: #{date}, and the month is: #{month}."
